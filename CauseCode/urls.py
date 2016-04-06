9d1a0b0912498bce4ed108aea217b76c21a6479e
Rahul Gosain <rahul.gosain94@gmail.com>
2016-04-06 17:58:18 +0530

    background image

--- CauseCode/urls.py
+++ CauseCode/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('polls.urls')),
+    url(r'^', include('polls.urls')),
     url(r'^polls/',include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
